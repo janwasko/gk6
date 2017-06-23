b356ceb397bc3ea9304392e6498f7e1399bd3049
Jan Waśko <janwasko@wp.pl>
2017-06-23 19:22:32 +0200

    Dodaj pliki projektów.

---
+++ GK_lab6/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace GK_lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.P1x = new System.Windows.Forms.TextBox();
+            this.P1y = new System.Windows.Forms.TextBox();
+            this.P2x = new System.Windows.Forms.TextBox();
+            this.P2y = new System.Windows.Forms.TextBox();
+            this.P3x = new System.Windows.Forms.TextBox();
+            this.P3y = new System.Windows.Forms.TextBox();
+            this.P4x = new System.Windows.Forms.TextBox();
+            this.P4y = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.P5x = new System.Windows.Forms.TextBox();
+            this.P5y = new System.Windows.Forms.TextBox();
+            this.P6x = new System.Windows.Forms.TextBox();
+            this.P6y = new System.Windows.Forms.TextBox();
+            this.P7x = new System.Windows.Forms.TextBox();
+            this.P7y = new System.Windows.Forms.TextBox();
+            this.P8x = new System.Windows.Forms.TextBox();
+            this.P8y = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(897, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Hermite\'a";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // P1x
+            // 
+            this.P1x.Location = new System.Drawing.Point(766, 12);
+            this.P1x.Name = "P1x";
+            this.P1x.Size = new System.Drawing.Size(100, 20);
+            this.P1x.TabIndex = 2;
+            // 
+            // P1y
+            // 
+            this.P1y.Location = new System.Drawing.Point(872, 13);
+            this.P1y.Name = "P1y";
+            this.P1y.Size = new System.Drawing.Size(100, 20);
+            this.P1y.TabIndex = 3;
+            // 
+            // P2x
+            // 
+            this.P2x.Location = new System.Drawing.Point(766, 38);
+            this.P2x.Name = "P2x";
+            this.P2x.Size = new System.Drawing.Size(100, 20);
+            this.P2x.TabIndex = 4;
+            // 
+            // P2y
+            // 
+            this.P2y.Location = new System.Drawing.Point(872, 39);
+            this.P2y.Name = "P2y";
+            this.P2y.Size = new System.Drawing.Size(100, 20);
+            this.P2y.TabIndex = 5;
+            // 
+            // P3x
+            // 
+            this.P3x.Location = new System.Drawing.Point(766, 65);
+            this.P3x.Name = "P3x";
+            this.P3x.Size = new System.Drawing.Size(100, 20);
+            this.P3x.TabIndex = 6;
+            // 
+            // P3y
+            // 
+            this.P3y.Location = new System.Drawing.Point(872, 65);
+            this.P3y.Name = "P3y";
+            this.P3y.Size = new System.Drawing.Size(100, 20);
+            this.P3y.TabIndex = 7;
+            // 
+            // P4x
+            // 
+            this.P4x.Location = new System.Drawing.Point(766, 91);
+            this.P4x.Name = "P4x";
+            this.P4x.Size = new System.Drawing.Size(100, 20);
+            this.P4x.TabIndex = 8;
+            // 
+            // P4y
+            // 
+            this.P4y.Location = new System.Drawing.Point(872, 91);
+            this.P4y.Name = "P4y";
+            this.P4y.Size = new System.Drawing.Size(100, 20);
+            this.P4y.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(897, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Bezier";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(896, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(897, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // P5x
+            // 
+            this.P5x.Location = new System.Drawing.Point(714, 274);
+            this.P5x.Name = "P5x";
+            this.P5x.Size = new System.Drawing.Size(100, 20);
+            this.P5x.TabIndex = 13;
+            // 
+            // P5y
+            // 
+            this.P5y.Location = new System.Drawing.Point(872, 274);
+            this.P5y.Name = "P5y";
+            this.P5y.Size = new System.Drawing.Size(100, 20);
+            this.P5y.TabIndex = 14;
+            // 
+            // P6x
+            // 
+            this.P6x.Location = new System.Drawing.Point(677, 332);
+            this.P6x.Name = "P6x";
+            this.P6x.Size = new System.Drawing.Size(100, 20);
+            this.P6x.TabIndex = 15;
+            // 
+            // P6y
+            // 
+            this.P6y.Location = new System.Drawing.Point(848, 332);
+            this.P6y.Name = "P6y";
+            this.P6y.Size = new System.Drawing.Size(100, 20);
+            this.P6y.TabIndex = 16;
+            // 
+            // P7x
+            // 
+            this.P7x.Location = new System.Drawing.Point(697, 376);
+            this.P7x.Name = "P7x";
+            this.P7x.Size = new System.Drawing.Size(100, 20);
+            this.P7x.TabIndex = 17;
+            // 
+            // P7y
+            // 
+            this.P7y.Location = new System.Drawing.Point(848, 376);
+            this.P7y.Name = "P7y";
+            this.P7y.Size = new System.Drawing.Size(100, 20);
+            this.P7y.TabIndex = 18;
+            // 
+            // P8x
+            // 
+            this.P8x.Location = new System.Drawing.Point(723, 428);
+            this.P8x.Name = "P8x";
+            this.P8x.Size = new System.Drawing.Size(100, 20);
+            this.P8x.TabIndex = 19;
+            // 
+            // P8y
+            // 
+            this.P8y.Location = new System.Drawing.Point(871, 438);
+            this.P8y.Name = "P8y";
+            this.P8y.Size = new System.Drawing.Size(100, 20);
+            this.P8y.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 712);
+            this.Controls.Add(this.P8y);
+            this.Controls.Add(this.P8x);
+            this.Controls.Add(this.P7y);
+            this.Controls.Add(this.P7x);
+            this.Controls.Add(this.P6y);
+            this.Controls.Add(this.P6x);
+            this.Controls.Add(this.P5y);
+            this.Controls.Add(this.P5x);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.P4y);
+            this.Controls.Add(this.P4x);
+            this.Controls.Add(this.P3y);
+            this.Controls.Add(this.P3x);
+            this.Controls.Add(this.P2y);
+            this.Controls.Add(this.P2x);
+            this.Controls.Add(this.P1y);
+            this.Controls.Add(this.P1x);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox P1x;
+        private System.Windows.Forms.TextBox P1y;
+        private System.Windows.Forms.TextBox P2x;
+        private System.Windows.Forms.TextBox P2y;
+        private System.Windows.Forms.TextBox P3x;
+        private System.Windows.Forms.TextBox P3y;
+        private System.Windows.Forms.TextBox P4x;
+        private System.Windows.Forms.TextBox P4y;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox P5x;
+        private System.Windows.Forms.TextBox P5y;
+        private System.Windows.Forms.TextBox P6x;
+        private System.Windows.Forms.TextBox P6y;
+        private System.Windows.Forms.TextBox P7x;
+        private System.Windows.Forms.TextBox P7y;
+        private System.Windows.Forms.TextBox P8x;
+        private System.Windows.Forms.TextBox P8y;
+    }
+}
+
---
+++ GK_lab6/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GK_lab6
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Graphics g;
+        private System.Drawing.Pen pen1 = new System.Drawing.Pen(Color.Black, 1);
+        private System.Drawing.Pen pen_red = new System.Drawing.Pen(Color.Red, 1);
+        private System.Drawing.Pen pen_gray = new System.Drawing.Pen(Color.Gray, 1);
+        private SolidBrush brush1 = new SolidBrush(Color.Red);
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            P1x.Text = "-200";
+            P1y.Text = "50";
+            P2x.Text = "-150";
+            P2y.Text = "-50";
+            P3x.Text = "-100";
+            P3y.Text = "50";
+            P4x.Text = "-50";
+            P4y.Text = "-50";
+            P5x.Text = "0";
+            P5y.Text = "50";
+            P6x.Text = "50";
+            P6y.Text = "-50";
+            P7x.Text = "100";
+            P7y.Text = "50";
+            P8x.Text = "150";
+            P8y.Text = "-50";
+        }
+        private void drawPoint(int x, int y)
+        {
+            g.DrawEllipse(pen_red, x - 1 + pictureBox1.Width / 2, -y - 1 + pictureBox1.Height / 2, 3, 3);
+            g.FillEllipse(brush1, x - 1 + pictureBox1.Width / 2, -y - 1 + pictureBox1.Height / 2, 3, 3);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int P1_x, P1_y, P4_x, P4_y, R1_x, R1_y, R4_x, R4_y;
+            double t, t2, t3;
+            
+            int.TryParse(P1x.Text, out P1_x);
+            int.TryParse(P1y.Text, out P1_y);
+            int.TryParse(P2x.Text, out P4_x);
+            int.TryParse(P2y.Text, out P4_y);
+            int.TryParse(P3x.Text, out R1_x);
+            int.TryParse(P3y.Text, out R1_y);
+            int.TryParse(P4x.Text, out R4_x);
+            int.TryParse(P4y.Text, out R4_y);
+            int n = 100;
+            int prev_x, prev_y;
+            double x = P1_x, y = P1_y;
+            double delta = 1.0 / n;
+            for (int i = 0; i < n; i++)
+            {
+                t = delta * i;
+                t2 = t * t;
+                t3 = t2 * t;
+                prev_x = (int)x;
+                prev_y = (int)y;
+                x = (int)((2 * t3 - 3 * t2 + 1) * P1_x + (-2 * t3 + 3 * t2) * P4_x + (t3 - 2 * t2 + t) * R1_x + (t3 - t2) * R4_x);
+                y = (int)((2 * t3 - 3 * t2 + 1) * P1_y + (-2 * t3 + 3 * t2) * P4_y + (t3 - 2 * t2 + t) * R1_y + (t3 - t2) * R4_y);
+
+                g.DrawLine(pen1, prev_x + pictureBox1.Width / 2, prev_y + pictureBox1.Height / 2, (int)x + pictureBox1.Width / 2, (int)y + pictureBox1.Height / 2);
+            }
+            //drawPoint(P1_x, P1_y);
+            //drawPoint(P4_x, P4_y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int P1_x, P1_y, P2_x, P2_y, R1_x, R1_y, R2_x, R2_y;
+            double t, t2, t3;
+            int.TryParse(P1x.Text, out P1_x);
+            int.TryParse(P1y.Text, out P1_y);
+            int.TryParse(P2x.Text, out P2_x);
+            int.TryParse(P2y.Text, out P2_y);
+            int.TryParse(P3x.Text, out R1_x);
+            int.TryParse(P3y.Text, out R1_y);
+            int.TryParse(P4x.Text, out R2_x);
+            int.TryParse(P4y.Text, out R2_y);
+
+
+            int n = 100;
+            int prev_x, prev_y;
+            double x = P1_x, y = P1_y;
+            double delta = 1.0 / n;
+            for (int i = 0; i < n; i++)
+            {
+                t = delta * i;
+                t2 = t * t;
+                t3 = t2 * t;
+                prev_x = (int)x;
+                prev_y = (int)y;
+                x = (int)((1 - t) * (1 - t) * (1 - t) * P1_x + 3 * t * (1 - t) * (1 - t) * P2_x + 3 * t2 * (1 - t) * R1_x + (t3) * R2_x);
+                y = (int)((1 - t) * (1 - t) * (1 - t) * P1_y + 3 * t * (1 - t) * (1 - t) * P2_y + 3 * t2 * (1 - t) * R1_y + (t3) * R2_y);
+
+                g.DrawLine(pen1, prev_x + pictureBox1.Width / 2, prev_y + pictureBox1.Height / 2, (int)x + pictureBox1.Width / 2, (int)y + pictureBox1.Height / 2);
+            }
+            //drawPoint(P1_x, P1_y);
+            //drawPoint(P2_x, P2_y);
+            //drawPoint(R1_x, R1_y);
+            //drawPoint(R2_x, R2_y);
+        }
+            
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int P1_x, P1_y, P2_x, P2_y, P3_x, P3_y, P4_x, P4_y, P5_x, P5_y, P6_x, P6_y, P7_x, P7_y, P8_x, P8_y;
+
+
+            int.TryParse(P1x.Text, out P1_x);
+            int.TryParse(P1y.Text, out P1_y);
+            int.TryParse(P2x.Text, out P2_x);
+            int.TryParse(P2y.Text, out P2_y);
+            int.TryParse(P3x.Text, out P3_x);
+            int.TryParse(P3y.Text, out P3_y);
+            int.TryParse(P4x.Text, out P4_x);
+            int.TryParse(P4y.Text, out P4_y);
+            int.TryParse(P5x.Text, out P5_x);
+            int.TryParse(P5y.Text, out P5_y);
+            int.TryParse(P6x.Text, out P6_x);
+            int.TryParse(P6y.Text, out P6_y);
+            int.TryParse(P7x.Text, out P7_x);
+            int.TryParse(P7y.Text, out P7_y);
+            int.TryParse(P8x.Text, out P8_x);
+            int.TryParse(P8y.Text, out P8_y);
+
+            int[] pointsX = { P1_x, P2_x, P3_x, P4_x, P5_x, P6_x, P7_x, P8_x };
+            int[] pointsY = { P1_y, P2_y, P3_y, P4_y, P5_y, P6_y, P7_y, P8_y };
+            float n = 100;
+            float x = 1, y = 1;
+            float a = 1, b = 1;
+            for (int i = 3; i < 8; i++)
+            {
+
+                for (float t = 0; t < 1.0; t += (1 / n))
+                {
+
+                    a = (pow(1 - t, 3) / 6 * pointsX[i - 3] + (3 * pow(t, 3) - 6 * pow(t, 2) + 4) / 6 * pointsX[i - 2] + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) / 6 * pointsX[i - 1] + pow(t, 3) / 6 * pointsX[i]);
+                    b = (pow(1 - t, 3) / 6 * pointsY[i - 3] + (3 * pow(t, 3) - 6 * pow(t, 2) + 4) / 6 * pointsY[i - 2] + (-3 * pow(t, 3) + 3 * pow(t, 2) + 3 * t + 1) / 6 * pointsY[i - 1] + pow(t, 3) / 6 * pointsY[i]);
+                    x = a;
+                    y = b;
+                    g.DrawLine(pen1, x + pictureBox1.Width / 2, -y + pictureBox1.Height / 2, (int)a + pictureBox1.Width / 2, (int)-b + pictureBox1.Height / 2);
+
+                }
+
+            }
+            //g.DrawLine(pen1, 20, pictureBox1.Height / 2, pictureBox1.Width - 20, pictureBox1.Height / 2);
+            //g.DrawLine(pen1, pictureBox1.Width / 2, 20, pictureBox1.Width / 2, pictureBox1.Height - 20);
+            drawPoint(P1_x, P1_y);
+            drawPoint(P2_x, P2_y);
+            drawPoint(P3_x, P3_y);
+            drawPoint(P4_x, P4_y);
+            drawPoint(P5_x, P5_y);
+            drawPoint(P6_x, P6_y);
+            drawPoint(P7_x, P7_y);
+            drawPoint(P8_x, P8_y);
+        }
+        private float pow(float x, int power)
+        {
+            float ret = 1;
+            bool flip = false;
+            if (power < 0)
+            {
+                power *= -1;
+                flip = true;
+            }
+            for (int p = 0; p < power; ++p)
+            {
+                ret *= x;
+            }
+            return flip ? 1 / ret : ret;
+        }
+    }
+}
